facb99a5261404bef8f0021f3b0d34e98bcfdedf
lykmapipo <lallyelias87@gmail.com>
2020-08-18 20:06:10 +0300

    feat(every): define interface

--- src/index.js
+++ src/index.js
@@ -112,6 +112,29 @@ export const enableExpiryNotifications = (done) => {
 };
 
 /**
+ * @function every
+ * @name every
+ * @description Schedule a function to execute at specified interval
+ * @param {string} interval Valid schedule in `human-readable` or `cron` format
+ * @param {string} name Valid schedule name
+ * @param {Function} handler Valid function to invoke per recurrence rules
+ * @param {object} [optns] Extra data and metadata passed when invoking handler
+ * @author lally elias <lallyelias87@gmail.com>
+ * @license MIT
+ * @since 0.1.0
+ * @version 0.1.0
+ * @static
+ * @public
+ * @example
+ *
+ * every('2 seconds', 'sendEmail', (done) => { ... });
+ *
+ * every('*\/2 * * * * *', 'sendEmail', (done) => { ... });
+ *
+ */
+export const every = (/* interval, name, handler, optns */) => {};
+
+/**
  * @function quit
  * @name quit
  * @description Quit and restore redis clients states
